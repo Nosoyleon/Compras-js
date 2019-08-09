26fae44964de830ee59bba81616051760990cb86
León Ceballos <leon.ceballos@wolox.co>
2019-08-08 20:06:01 -0500

    inde object file

---
+++ index-object.js
@@ -0,0 +1,6 @@
+const products = require("./products");
+const list = require("./shopping-list");
+
+products.forEach(item => console.log('products', item));
+list.forEach(item => console.log('list', item));
+
