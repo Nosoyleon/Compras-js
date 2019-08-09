cc19d0b4a7daae5fe9b1bbb86bbad1dbad80aaf3
León Ceballos <leon.ceballos@wolox.co>
2019-08-08 19:31:39 -0500

    shopping list

---
+++ shopping-list.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    id: 1,
+    quantity: 1
+  },
+  {
+    id: 2,
+    quantity: 20
+  },
+  {
+    id: 6,
+    quantity: 10
+  },
+  {
+    id: 3,
+    quantity: 2
+  },
+  {
+    id: 9,
+    quantity: 6
+  },
+  {
+    id: 8,
+    quantity: 7
+  },
+  {
+    id: 4,
+    quantity: 1
+  }
+];
